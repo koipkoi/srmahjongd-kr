16c410bff874c13917b4ad46dbe16fe95362da29
Hwang Ryuha <hrh6914@gmail.com>
2022-03-22 17:20:08 +0900

    대사 추출
    - script_0.json : 모든 텍스트
    - script_1.json : 오프닝 텍스트

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @rem 사본 생성
 copy "baserom.gba" "srmahjongd-kr.gba"
 
+@rem 텍스트 처리
+@del text.g.asm
+tools\textproc charmap.tbl text ko 087c6500 text.g.asm
+
 @rem 컴파일
 tools\xkas -o "srmahjongd-kr.gba" main.asm
 
