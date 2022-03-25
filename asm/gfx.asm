67dd88f996fbcd87bcbd6c001d5c0e36f02c7236
Hwang Ryuha <hrh6914@gmail.com>
2022-03-25 09:11:25 +0900

    메인 메뉴 그래픽 추출

--- asm/gfx.asm
+++ asm/gfx.asm
@@ -1,3 +1,4 @@
 arch gba.thumb
 
+incsrc asm/gfx_main_menu.asm
 incsrc asm/gfx_game.asm
---
+++ asm/gfx_main_menu.asm
@@ -0,0 +1,4 @@
+arch gba.thumb
+
+org $08408004 ; incbin gfx/main_menu/menus_21x6.4bpp
+org $08409610 ; incbin gfx/main_menu/logo_32x9.4bpp
